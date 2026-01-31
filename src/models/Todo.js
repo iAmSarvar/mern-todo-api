0a548a5af256e249031ab159c3c4ce74551a1a29
iAmSarvar <iamsarvarsultanov@gmail.com>
2026-01-31 23:55:28 +0500

    feat: add todo model

---
+++ src/models/Todo.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 2,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  timestamps: true,
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
