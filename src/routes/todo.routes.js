0030eaa95eaa18872c4d1a938b002e78aa933427
iAmSarvar <iamsarvarsultanov@gmail.com>
2026-02-15 17:51:57 +0500

    refactor: rename validateBody middleware for consistency

--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const todoController = require("../controllers/todo.controller");
-const validateBody = require("../middlewares/validatebody");
+const validateBody = require("../middlewares/validateBody");
 
 const router = express.Router();
 
